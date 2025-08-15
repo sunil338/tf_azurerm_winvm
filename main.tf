ce0c1657e6bb76af6fd87ba2768fa2c4fae3b66b
Sunil <sunilmuradi007@gmail.com>
2025-08-16 00:44:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,6 +52,7 @@ resource "azurerm_network_security_group" "nsg" {
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
+    source_port_range          = "*"
     destination_port_range     = "3389"
     source_address_prefix      = var.allowed_ip
     destination_address_prefix = "*"
@@ -63,6 +64,7 @@ resource "azurerm_network_security_group" "nsg" {
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
+    source_port_range          = "*"
     destination_port_range     = "5986"
     source_address_prefix      = var.allowed_ip
     destination_address_prefix = "*"
