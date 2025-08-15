ee220db6129cc431c7e97ee5ff1fee28595a5a49
Sunil <sunilmuradi007@gmail.com>
2025-08-16 00:46:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,6 +15,10 @@ terraform {
 
 provider "azurerm" {
   features {}
+client_id="3c8da5d7-001d-4ef2-96e8-13368b5a48d6"
+client_secret="31h8Q~T0DeKBouJvyUb6F5tDJ9_AFYYTV3TzAbjH"
+tenant_id="7e1b4521-93b6-473d-8e77-cf32c35bff05"
+subscription_id="34a1c0d5-ab81-462c-89db-795e1572cd70"
 }
 
 variable "admin_password" { sensitive = true }
